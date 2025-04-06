c2205f50567f45b95a7e2cadfdd18eb0b412ca9a
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-04-05 19:25:33 -0600

    Created initial structure of files

---
+++ backend/recipe.py
@@ -0,0 +1,6 @@
+# Recipe class and its methods
+
+def __init__(self, Title, description, instructions):
+        self.Title = Title
+        self.description = description  # should be a list of strings
+        self.instructions = instructions
