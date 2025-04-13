d52730e49ee626af55e08d31230bf3225a96cd47
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-04-13 14:35:14 -0600

    Created API calls and class for ingredient

--- db.py
+++ db.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import psycopg2
 from urllib.parse import urlparse
 
+from classes.ingredient import Ingredient
+
 load_dotenv()
 
 def get_connection():
@@ -35,3 +37,63 @@ def search_recipe(recipe_id):
     except Exception as e:
         print(f"Search Recipe Error: {e}")
         return None
+    
+def get_user(userid):
+    # placeholder function to get the user from their userid
+    return [1]
+
+
+def create_ingredient(ingred, userID):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        user = get_user(userID)
+        if user[-1] == 1:
+            query = """
+                INSERT INTO ingredient (name, calories, unit, moderatorid)
+                VALUES (%s, %s, %s, %s);
+            """
+            cursor.execute(query, (ingred[1], ingred[2], ingred[3], userID))
+        else:
+            print("Access Denied: User is not an admin")
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(f"Create Ingredient Error: {e}")
+        return None
+    
+
+def view_ingredient(ingredientID):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        query = """
+            SELECT ingredientid, name, calories, unit, moderatorid
+            FROM ingredient
+            WHERE IngredientID = %s;
+        """
+        cursor.execute(query, (ingredientID,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return Ingredient(result[0], result[1], result[2], result[3], result[4])
+    except Exception as e:
+        print(f"View Ingredient Error: {e}")
+    
+def delete_ingredient(ingredientID):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        query = """
+            DELETE FROM ingredient
+            WHERE ingredientid = %s;
+        """
+        cursor.execute(query, (ingredientID,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"Delete Ingredient Error: {e}")
