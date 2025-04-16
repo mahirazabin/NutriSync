999beb3ead98e484272f1c0dec3cb47e12e87152
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-04-16 14:15:09 -0600

    added basic page for creating recipes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import LoginPage from './pages/Login';
+import CreateRecipe from './pages/createRecipe';
 
 function App() {
   return <LoginPage />;
+}
+
+function createRecipe() {
+  return <CreateRecipe />;
 }
 
 export default App;
